3e929be1713f4a560f34cd95b0a8a5abd9c54103
Debjyoti mondal <91774344+debjyoti-m28@users.noreply.github.com>
2022-12-26 14:34:55 +0530

    Completed city repo

--- src/repository/city-repository.js
+++ src/repository/city-repository.js
@@ -18,10 +18,36 @@ class CityRepository {
                     id: cityId
                 }
             });
+            return true;
         } catch (error){
+            console.log("Something went wrong int repository layter");
             throw (error); 
         }
-    }    
+    } 
+    
+    async getCity( cityId ){
+        try{
+            const city = await City.findByPk(cityId);
+            return city;
+        } catch (error){
+            console.log("Something went wrong int repository layter");
+            throw (error); 
+        }
+    } 
+
+    async updateCity( cityId, data ){
+        try{
+            const city = await City.update(data, {
+                where: {
+                    id: cityId
+                }
+            });
+            return city;
+        } catch (error){
+            console.log("Something went wrong int repository layter");
+            throw (error); 
+        }
+    } 
 }
 
 module.exports = CityRepository;
