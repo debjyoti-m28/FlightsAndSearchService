2f8350005fa066e73f7e0f7bcae08695b965e148
Debjyoti mondal <91774344+debjyoti-m28@users.noreply.github.com>
2022-12-28 17:54:29 +0530

    Added flight controller

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -1,11 +1,17 @@
 const express = require("express");
 const CityController = require("../../controllers/city-controller");
+const FlightController = require("../../controllers/flight-controller");
 const router = express.Router();
 
+//city
 router.post('/city', CityController.create);
 router.delete('/city/:id', CityController.destroy);
 router.get('/city/:id', CityController.get);
 router.get('/city', CityController.getAll);
 router.patch('/city/:id', CityController.update);
+
+
+//flight
+router.post('/flights', FlightController.create);
 
 module.exports = router;
